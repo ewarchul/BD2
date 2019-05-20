2c596858c186941d56c5874266e6e1c231a27556
Gawron <piotr.gaw@gmail.com>
2019-05-21 00:17:58 +0200

    dodane wrzucanie organizacji i osob fizycznych

--- init.sh
+++ init.sh
@@ -4,3 +4,5 @@ sqlite3 $BAZA < init.sql
 (echo .separator ,; echo .import data/dzialy.csv dzial) | sqlite3 $BAZA
 (echo .separator ,; echo .import data/pracownicy.csv pracownik) | sqlite3 $BAZA
 (echo .separator ,; echo .import data/umowa.csv umowa) | sqlite3 $BAZA
+(echo .separator ,; echo .import data/organizacje.csv organizacja) | sqlite3 $BAZA
+(echo .separator ,; echo .import data/osoba-fizyczna.csv osoba_fizyczna) | sqlite3 $BAZA
