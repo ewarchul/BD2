2c69f9c282456854f4688ab1dca1cc2c9dc6fa3d
Gawron <piotr.gaw@gmail.com>
2019-05-21 00:15:25 +0200

    Dodany import pracownikow

--- init.sh
+++ init.sh
@@ -3,3 +3,4 @@ BAZA=mydb
 sqlite3 $BAZA < init.sql 
 (echo .separator ,; echo .import data/dzialy.csv dzial) | sqlite3 $BAZA
 (echo .separator ,; echo .import data/pracownicy.csv pracownik) | sqlite3 $BAZA
+(echo .separator ,; echo .import data/umowa.csv umowa) | sqlite3 $BAZA
