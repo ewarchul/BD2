2cbc6c2381622884808616798ccb0647c8f84ce8
Gawron <piotr.gaw@gmail.com>
2019-05-21 01:27:27 +0200

    Dodane rodzaje umowy, uprawnienia, osoby uprawnione

--- init.sh
+++ init.sh
@@ -7,3 +7,6 @@ sqlite3 $BAZA < init.sql
 (echo .separator ,; echo .import data/organizacje.csv organizacja) | sqlite3 $BAZA
 (echo .separator ,; echo .import data/osoba-fizyczna.csv osoba_fizyczna) | sqlite3 $BAZA
 (echo .separator ,; echo .import data/pozycja.csv pozycja) | sqlite3 $BAZA
+(echo .separator ,; echo .import data/rodzaj-uprawnienia.csv rodzaj_uprawnienia) | sqlite3 $BAZA
+(echo .separator ,; echo .import data/osoba-uprawniona.csv osoba_uprawniona) | sqlite3 $BAZA
+(echo .separator ,; echo .import data/rodzaj-umowy.csv rodzaj_umowy) | sqlite3 $BAZA
