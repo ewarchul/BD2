d106aadec64f10146fd9973e65148c2be414d028
warbarbye <ewarchul@gmail.com>
2019-06-11 00:38:16 +0200

    app fix

--- src/app.py
+++ src/app.py
@@ -29,8 +29,8 @@ class ViewWindow(QWidget):
         db.open()
         query = QSqlQuery(db)
         if cur_txt == 'pracownik':
-            query_residual = 'poziom_dostepu = {} and imie = {} and nazwisko = {} and id_pracownika = {} and stanowisko = {} '.format(self.filter_acclvl.text(), self.filter_worker_name.text(), self.filter_worker_surname.text(), self.filter_worker_id.text(), self.filter_worker_job.text())
-            key_val = re.sub('X', ' and ','X'.join(re.findall('\w+ = \w+' ,query_residual)))
+            query_residual = 'poziom_dostepu = \'{}\' and imie = \'{}\' and nazwisko = \'{}\' and id_pracownika = \'{}\' and stanowisko = \'{}\' '.format(self.filter_acclvl.text(), self.filter_worker_name.text(), self.filter_worker_surname.text(), self.filter_worker_id.text(), self.filter_worker_job.text())
+            key_val = re.sub('X', ' and ','X'.join(re.findall('\w+ = \'\w+\'' ,query_residual)))
             query.prepare('select * from pracownik where ' + key_val)
         elif cur_txt == 'podmiot_zewnetrzny':
             query.prepare('select * from {} where {}')
